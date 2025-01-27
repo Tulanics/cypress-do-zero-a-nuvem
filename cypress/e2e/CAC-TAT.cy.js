3b6fd048988303b2183190286125c5bbde19b462
Tulanics <tulanics@gmail.com>
2025-01-27 13:39:46 -0300

    Lesson 7

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -204,4 +204,20 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       })
   })
 
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'target', '_blank')
+      .and('have.attr', 'href', 'privacy.html')
+    
+  })
+  
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+
+  })
+
 })
